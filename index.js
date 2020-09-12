a265a4d6bee142e8d4ff042cc699b6c0f804a775
Aslak Hollund <aslakhol@gmail.com>
2020-09-13 00:20:50 +0200

    reorganize logs

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const github = require("@actions/github");
 try {
   console.log(`automerge action log`);
   const pullRequest = github.context.payload["pull_request"];
-  // console.log(pullRequest);
-
   const main_branch = pullRequest.main_branch;
   const head = pullRequest.head;
-
   const id = github.context.payload["repository"].id;
 
-  console.log(id, head, main_branch);
+  console.log(`id: ${id}`);
+  console.log(`head: ${head}`);
+  console.log(`main_branch: ${main_branch}`);
+
+  console.log(github.context.payload);
 } catch (error) {
   core.setFailed(error.message);
 }
