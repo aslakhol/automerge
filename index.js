315cfa19bf4c368f27d559ca85d798066fde8c86
Aslak Hollund <aslakhol@gmail.com>
2020-09-13 14:09:40 +0200

    add token input and cleanup

--- index.js
+++ index.js
@@ -2,22 +2,12 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  console.log(`automerge action log`);
   const pullRequest = github.context.payload["pull_request"];
-  const base = pullRequest.base.ref;
-  const head = pullRequest.head.ref;
-  const repo_id = github.context.payload["repository"].node_id;
   const pr_id = pullRequest.node_id;
 
-  console.log(`repo id: ${repo_id}`);
   console.log(`pr id: ${pr_id}`);
-  console.log(`head: ${head}`);
-  console.log(`base: ${base}`);
-
-  // console.log(github.context.payload);
 
   const repoToken = core.getInput("repo-token");
-
   const octokit = github.getOctokit(repoToken);
 
   octokit
